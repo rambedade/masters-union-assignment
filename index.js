148efd6b4f08fb262013a6cb83b0d5a388035c61
rambedade <rambedade2308@gmail.com>
2025-01-28 14:09:01 +0530

    worked on responsive side

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const menuToggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
 menuToggle.addEventListener("click", () => {
-    menu.classList.toggle("show");
+    menu.classList.toggle("hide");
 });
